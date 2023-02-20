e50cffa2bb403465f0f3dfdf8f1599f787583774
pittachalk <>
2023-02-20 18:58:53 +0000

    modify search_hits to use Path module

--- script/02-scan_hits.py
+++
@@ -1,7 +0,0 @@
-# %%
-from pathlib import Path
-REPO_DIR = Path(__file__).resolve()
-
-# f"BIOGRID-ORCS-SCREEN_{id}-1.1.13.screen.tab.txt"
-
-# %%
--- script/02_search_hits.py
+++ script/02_search_hits.py
@@ -8,21 +8,20 @@
 import pandas as pd
 import glob
 import tqdm
-#from pathlib import Path
-
+from pathlib import Path
 from BioGRID import hit_search
 
-directory = '/Users/alymakhlouf/Desktop/biospark/data/'
+directory = Path(__file__).resolve().parent.parent.joinpath("data/")
+threshold = 10 # filter top % of each hit list
+
 
 # SELECT ONLY TIMECOURSE, INHIBITION/KNOCKOUT EXPERIMENTS
-
-df = pd.read_csv(directory + 'index_file_polished.csv')
+df = pd.read_csv(directory.joinpath('index_file_polished.csv'))
 df = df[df['EXPERIMENTAL_SETUP'] == 'Timecourse']
 df = df[df['LIBRARY_METHODOLOGY'] != 'Activation']
 
+
 # EXTRACT UNIQUE AND NON-UNIQUE, SINGLE AND MULTIPLE CELL TYPES
-
-
 cell_type_broad = [i for i in df.CELL_TYPE_BROAD.unique()]
 cell_type_broad_single = [i for i in set([i for j in [i.split(' / ') 
                     for i in df.CELL_TYPE_BROAD.unique()] for i in j])]
@@ -36,17 +35,12 @@ cell_type_broad_non_unique_subset = [x for x in set(cell_type_broad_non_unique)
 cell_type_broad_non_unique = [i for i in set([j for j in cell_type_broad_single 
                           for k in cell_type_broad_multiple if j in k])]    
 
-screen_list = glob.glob(directory + "BIOGRID-ORCS-ALL-homo_sapiens-1.1.13.screens/*.txt")
+screen_list = glob.glob(str(directory.joinpath("BIOGRID-ORCS-ALL-homo_sapiens-1.1.13.screens/*.txt")))
 
 
 # GROUP CELLS BASED ON UNIQUE (SINGLE & MULTIPLE) BROAD CELL TYPE
-
-
-x = 10 # filter top x% of each hit list
-
 cell_hit_dict = {}
 cell_alias_dict = {}
-
 
 for cell_broad in tqdm.tqdm(cell_type_broad_all[0:]):
     
@@ -59,11 +53,10 @@ for cell_broad in tqdm.tqdm(cell_type_broad_all[0:]):
     if len(group) == 0:
         continue
     
-    cell_hit_dict[cell_broad_key], cell_alias_dict[cell_broad_key] = hit_search(screen_list,x,group)
+    cell_hit_dict[cell_broad_key], cell_alias_dict[cell_broad_key] = hit_search(screen_list,threshold,group)
 
 
 # GROUP CELLS BASED ON NON-UNIQUE BROAD CELL TYPE
-
 
 for cell_broad in tqdm.tqdm(cell_type_broad_non_unique[0:]):
     
@@ -73,15 +66,13 @@ for cell_broad in tqdm.tqdm(cell_type_broad_non_unique[0:]):
 
     group = df[df['CELL_TYPE_BROAD'].str.contains(cell_broad)]
     
-    cell_hit_dict[cell_broad_key], cell_alias_dict[cell_broad_key] = hit_search(screen_list,x,group)
+    cell_hit_dict[cell_broad_key], cell_alias_dict[cell_broad_key] = hit_search(screen_list,threshold,group)
 
     # delete redundant (duplicated) entries
     if cell_broad in cell_type_broad_non_unique_subset:
         cell_hit_dict.pop(cell_broad_key)
         cell_alias_dict.pop(cell_broad_key)
 
-pd.DataFrame.from_dict(cell_hit_dict, orient='index').to_csv(directory + 'hit_list.csv')        
-pd.DataFrame.from_dict(cell_alias_dict, orient='index').to_csv(directory + 'alias_list.csv')  
+pd.DataFrame.from_dict(cell_hit_dict, orient='index').to_csv(directory.joinpath('hit_list.csv'))
+pd.DataFrame.from_dict(cell_alias_dict, orient='index').to_csv(directory.joinpath('alias_list.csv'))
 
-#Path(__file__).resolve()
-#Path(__file__).resolve().parent.parent.joinpath("data/")
