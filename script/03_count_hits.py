5581e0eb62d6de2f86f48e7b5ea2ddf92459e923
pittachalk <>
2023-02-21 12:06:58 +0000

    update count_hits.py to deal with cell types with / and spaces, beautify plots

--- script/03_count_hits.py
+++ script/03_count_hits.py
@@ -1,71 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # %%
 import pandas as pd
 import numpy as np
 import plotly.express as px
 import tqdm
+import re
 from pathlib import Path
 
-# %%
 directory = Path(__file__).resolve().parent.parent.joinpath("data/")
 
-
-
-# %%
+# read the compiled hit list
+# setting the first column containing the broad cell types as the index
 df = pd.read_csv(directory.joinpath('hit_list.csv'), index_col=0)
 
-
-
-
 # %%
+# for each broad cell type...
 for i in tqdm.tqdm(range(df.shape[0])):
+
+    # get occurences of genes 
     current_hits = df.iloc[i]
     current_hits = current_hits.dropna()
 
+    # retrieve cell type, remove spaces and substituting slashes
     current_cell = df.index[i]
+    current_cell = re.sub("/", '-', current_cell)
+    current_cell = re.sub(" ", '', current_cell)
 
+    # retrieve the number of times each gene was a hit as a dataframe
     genes, counts = np.unique(current_hits, return_counts=True)
+    df_subset = pd.DataFrame({'gene': genes, 'num_hits': counts})
+    df_subset['num_hits'] = df_subset['num_hits'].astype("category")
 
-    df_subset = pd.DataFrame({'gene': genes, 'count': counts})
-
-    df_subset['count'] = df_subset['count'].astype("category")
-
-    tmp_df = (df_subset.groupby('count')
-            .apply(lambda dataframe: ', '.join(dataframe.gene))
-            .reset_index()
-            .rename(columns={0: "Genes"})
+    # for each category of num_hits, join the genes as a single string
+    tmp_df = (df_subset
+        .groupby('num_hits')
+        .agg({'gene': [lambda x: ', '.join(x), 'count']})
+    )
+    df_summary = (tmp_df
+        .loc[:, "gene"]
+        .rename(columns={'<lambda_0>': "Genes", 'count': "Frequency"})
+        .reset_index()
     )
 
-    df_subset = pd.merge(df_subset, tmp_df)
-
-
-    fig = px.bar(df_subset, 
-        x="count", color='Genes', hover_data={'count': False}
+    # create interactive bar chart to visualise this data 
+    fig = px.bar(df_summary, 
+        x="num_hits", y="Frequency", 
+        hover_data={'Genes': True, 'num_hits': False},
+        template="seaborn"
     )
     fig.update_layout(hovermode='x', 
-        xaxis_title="Number of Hits", yaxis_title="Frequency")
+        xaxis_title="Number of Hits", 
+        yaxis_title="Frequency",
+        title_text=f"Gene hits in {df.index[i]} screens",
+        title_font_size=30
+    )
     fig.update_traces(showlegend=False)
     fig.write_html(directory.joinpath(f"figure/hitHistogram_{current_cell}.html"))
 
-
-# %%
-df_subset = pd.DataFrame({'gene': genes, 'count': counts})
-
-tmp_df = (df_subset.groupby('count')
-          .apply(lambda dataframe: ', '.join(dataframe.gene))
-          .reset_index()
-          .rename(columns={0: "Genes"})
-)
-
-df_subset = pd.merge(df_subset, tmp_df)
-
-
-# %%
-fig = px.histogram(df_subset, 
-    x="count", color='Genes', hover_data={'count': False}
-)
-fig.update_layout(hovermode='x', 
-    xaxis_title="Number of Hits", yaxis_title="Frequency")
-fig.update_traces(showlegend=False)
-fig.write_html(directory.joinpath(f"figure/hitHistogram_{current_cell}.html"))
 
 # %%
